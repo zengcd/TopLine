0a4b8dff85b86240fb3725cd67a392fe8c65dc40
zengcd <2644903809@qq.com>
2020-01-08 10:13:23 +0800

    首页绘制

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>后台首页面展示</div>
+  <el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -8,4 +14,16 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-container{
+  height: 100%;
+  .el-aside{
+    background-color: rgb(50,55,69);
+  }
+.el-header{
+  background-color: orange;
+}
+.el-main{
+  background-color: lightgray;
+}
+}
 </style>
